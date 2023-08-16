60e396ee52ea463d97bb84f31dbe983f49571bd6
xLillium <nicolas.motillon@gmail.com>
2023-08-16 22:14:00 +0200

    Added <leader>lg keymap for LazyGit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,10 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+
+--------------------
+-- Plugin Keymaps --
+--------------------
+
+-- Lazy Git
+vim.keymap.set('n', '<leader>lg', ':LazyGit<CR>', { desc = 'Open [L]azy[G]it' })
