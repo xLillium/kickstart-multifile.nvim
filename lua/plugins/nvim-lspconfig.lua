0ba685e68d3fe48337da20568b256b510b4f5a3c
xLillium <nicolas.motillon@gmail.com>
2023-08-27 22:28:10 +0200

    Added lsp keymaps when attaching to a buffer

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -9,7 +9,7 @@ return {
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', tag = 'legacy', event = 'LspAttach', opts = {} },
+      { 'j-hui/fidget.nvim',       tag = 'legacy', event = 'LspAttach', opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
@@ -18,6 +18,33 @@ return {
       -- Setup neovim lua configuration
       require('neodev').setup()
       require('mason').setup()
+
+      --  This function gets run when an LSP connects to a particular buffer.
+      local on_attach = function(_, bufnr)
+        -- Enable LSP formatting on save
+        vim.api.nvim_create_autocmd('BufWritePost', {
+          callback = function()
+            vim.lsp.buf.format()
+          end
+        })
+        local opts = { noremap = true, silent = true }
+        local keymap = vim.api.nvim_buf_set_keymap
+        keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+        keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+        keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+        keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+        keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+        keymap(bufnr, 'n', 'gl', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+        keymap(bufnr, 'n', '<leader>lf', '<cmd>lua vim.lsp.buf.format{ async = true }<cr>', opts)
+        keymap(bufnr, 'n', '<leader>li', '<cmd>LspInfo<cr>', opts)
+        keymap(bufnr, 'n', '<leader>lI', '<cmd>LspInstallInfo<cr>', opts)
+        keymap(bufnr, 'n', '<leader>la', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+        keymap(bufnr, 'n', '<leader>lj', '<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>', opts)
+        keymap(bufnr, 'n', '<leader>lk', '<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>', opts)
+        keymap(bufnr, 'n', '<leader>lr', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        keymap(bufnr, 'n', '<leader>ls', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+        keymap(bufnr, 'n', '<leader>lq', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+      end
       local servers = {
         eslint = {},
         lua_ls = {
@@ -34,13 +61,15 @@ return {
       -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
       mason_lspconfig.setup_handlers {
-        function (server_name) -- default handler (optional)
-            require('lspconfig')[server_name].setup {
-                capabilities = capabilities,
-                settings = servers[server_name],
-                filetypes = (servers[server_name] or {}).filetypes,
-            }
+        function(server_name) -- default handler (optional)
+          require('lspconfig')[server_name].setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
+            settings = servers[server_name],
+            filetypes = (servers[server_name] or {}).filetypes,
+          }
         end,
       }
     end,
